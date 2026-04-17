d4691e9252ef4147ac4306300c6af75292e259f1
Maya Mathur <mmathur@stanford.edu>
2026-04-17 14:29:23 -0700

    update sim params to full-size N, etc., for inclusion in paper

--- simulation_study/Code/genSbatch_IWN.R
+++ simulation_study/Code/genSbatch_IWN.R
@@ -38,7 +38,7 @@ scen.params = tidyr::expand_grid(
   
   #rep.methods = "gold ; CC ; MICE-std ; Am-std ; MICE-ours ; MICE-ours-pred ; Am-ours",
   #rep.methods = "gold ; MVN-CC-ours ; Am-ours", 
-  rep.methods = "gold ; CC ; MICE-std ; MICE-ours ; MVN-CC-std ; MVN-CC-ours",
+  rep.methods = "gold ; CC ; MICE-std ; MICE-ours ; MVN-CC-std ; MVN-CC-ours ; Am-std ; Am-ours",
   
   model = "OLS",
   coef_of_interest = c( "(Intercept)", "A" ),
@@ -46,10 +46,10 @@ scen.params = tidyr::expand_grid(
   
   # MICE parameters
   # as on cluster
-  imp_m = 5,  # CURRENTLY SET LOW
-  imp_maxit = 100,
-  mice_method = NA, # use NA if you want mice's defaults
-  #mice_method = "norm", 
+  imp_m = 50,  # CURRENTLY SET LOW
+  imp_maxit = 200,
+  #mice_method = NA, # use NA if you want mice's defaults
+  mice_method = "norm",
   
   # # for quicker sims
   # imp_m = 5,
@@ -57,13 +57,15 @@ scen.params = tidyr::expand_grid(
   # N = c(100),
   
   #dag_name = c( "1B", "1D", "1G", "1H" ),
-  dag_name = c( "3A", "2A", "2B" )
+  dag_name = c( "2A", "2B" )
 )
+
 
 # remove combos that aren't implemented
 # DAGs where intercept is NOT implemented:
 scen.params = scen.params %>% filter( !(dag_name %in% c("1G", "1H", "1F") &
                                           coef_of_interest == "(Intercept)") )
+
 # DAGs where ONLY intercept is implemented:
 scen.params = scen.params %>% filter( !(dag_name %in% c("3A") & coef_of_interest != "(Intercept)") )
 
@@ -88,7 +90,7 @@ write.csv( scen.params, "scen_params.csv", row.names = FALSE )
 source("helper_IWN.R")
 
 # number of sbatches to generate (i.e., iterations within each scenario)
-n.reps.per.scen = 500  
+n.reps.per.scen = 5000  
 n.reps.in.doParallel = 100
 ( n.files = ( n.reps.per.scen / n.reps.in.doParallel ) * n.scen )
 
